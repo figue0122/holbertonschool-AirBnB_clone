8cae2ed0cb83650a21a46a90e885bf0b922e2371
figue0122 <you@example.com>
2023-11-02 19:42:01 -0400

    pycode error fix

--- console.py
+++ console.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-"""This module contains the entry point of the command interpreter."""
 
 import cmd
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from models import storage
-
 
 class HBNBCommand(cmd.Cmd):
-    """Class for the command interpreter."""
-
     prompt = '(hbnb) '
 
-    def do_quit(self, args):
-        """Quits the console."""
+    def do_quit(self, arg):
+        """Quit the command interpreter"""
         return True
 
-    def help_quit(self):
-        """Displays the help documentation for the 'quit' command."""
-        print("Quit command to exit the program")
-
-    def do_EOF(self, args):
-        """Handles the EOF command."""
+    def do_EOF(self, arg):
+        """Exit the command interpreter"""
         return True
-
-    def help_EOF(self):
-        """Displays the help documentation for the 'EOF' command."""
-        print("EOF command to exit the program")
 
     def emptyline(self):
-        """Does nothing when receiving an empty line followed by ENTER."""
+        """Do nothing when an empty line is entered"""
         pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
