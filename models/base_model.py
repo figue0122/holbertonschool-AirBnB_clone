425dc8e3118c28e447fc363ccd6307b33b27c5dd
figue0122 <you@example.com>
2023-11-02 14:46:00 -0400

    to_dict() method added

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/sur/bin/python3
 """ Base_models Module"""
-
 import uuid
 from datetime import datetime
 
@@ -10,7 +9,7 @@ class BaseModel:
             if '__class__' in kwargs:
                 del kwargs['__class__']
             for key, value in kwargs.items():
-                if key in ['created_at', 'updated_at']:
+                if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
         else:
