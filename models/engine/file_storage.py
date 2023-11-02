bda2678fab49756072aa4abc3dbe86d72c7b7f4b
figue0122 <you@example.com>
2023-11-02 14:56:04 -0400

    file storage module up

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,3 +24,26 @@ class FileStorage:
             "State": State,
             "Amenity": Amenity
         }
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        with open(self.__file_path, 'w') as f:
+            dict_obj = {key: value.to_dict() for key, value in self.__objects.items()}
+            json.dump(dict_obj, f)
+
+    def reload(self):
+        if path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                objs = json.load(f)
+                for key, value in objs.items():
+                    cls_name = value["__class__"]
+                    cls = self.CLASS_DICT.get(cls_name)
+                    if cls:
+                        instance = cls(**value)
+                        self.__objects[key] = instance
