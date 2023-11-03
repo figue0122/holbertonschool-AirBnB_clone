4bf8cbae5224c4c193c478130d82e7a3738ab402
figue0122 <you@example.com>
2023-11-02 20:21:32 -0400

    city module

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""User module"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class inheriting BaseModel"""
+    state_id = ""
+    name = ""
+
+    def __str__(self):
+        """Returns a string representation of the Amenity instance."""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+    
