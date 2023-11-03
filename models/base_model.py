cb4a34116aa7104785dae1651e586200dcf1601b
figue0122 <you@example.com>
2023-11-03 02:49:27 -0400

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,58 @@
-#!/sur/bin/python3
-""" Base_models Module"""
+#!/usr/bin/python3
+"""Base_Models Module"""
+
 import uuid
 from datetime import datetime
+import models
+
 
 class BaseModel:
+    """BaseModels class"""
+
     def __init__(self, *args, **kwargs):
+        """Initialize BaseModels instance"""
         if kwargs:
-            if '__class__' in kwargs:
-                del kwargs['__class__']
+            # For each key in the dictionary, set the attribute with \
+            # the associated value.
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+                if key == "created_at" or key == "updated_at":
+                    # Convert string datetime to datetime object.
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
+                # Check if 'id' is not in kwargs, then assign it.
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+            # Check if 'created_at' is not in kwargs, then assign it.
+            if "created_at" not in kwargs.keys():
+                self.created_at = datetime.now()
+            # Check if 'updated_at' is not in kwargs, then assign it
+            if "updated_at" not in kwargs.keys():
+                self.updated_at = datetime.now()
+            if not kwargs:
+                models.storage.new(self)
         else:
+            """initialize variables"""
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format("BaseModel", self.id, self.__dict__)
+        """Returns class, Id and dictionary"""
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
+        """Saves current time to updated_at"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = "BaseModel"
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-
+        """Create a dictionary and save current data in it"""
+        class_dict = dict(self.__dict__)
+        class_dict["__class__"] = self.__class__.__name__
+        class_dict["created_at"] = self.created_at.isoformat()
+        class_dict["updated_at"] = self.updated_at.isoformat()
+        return class_dict
+    
 
