04fd4cdd929fe0cbd4b038eaf31537b8d92db2b5
figue0122 <you@example.com>
2023-11-02 20:23:45 -0400

    state user module

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""User module"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+    name = ""
+
+    def __str__(self):
+        """String representation of the State instance"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+    
