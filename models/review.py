7dc6f4826b98676ba3ef2649cca1ef8af608e6ac
figue0122 <you@example.com>
2023-11-02 20:24:56 -0400

    review class

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module for Review class """
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Class of review"""
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __str__(self):
+        """String representation of the Review instance"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+    
