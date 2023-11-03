14012b1d33effb46cac32eb972034249252ed2a5
figue0122 <you@example.com>
2023-11-02 20:06:11 -0400

    file storage script change

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 """FileStorage Module"""
 
-import json
-from os import path
-from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.amenity import Amenity
-
-
 class FileStorage:
     """FileStorage class to serialize and deserialize instances"""
     __file_path = "file.json"
     __objects = {}
-    CLASS_DICT = {"BaseModel": BaseModel,
-                 "User": User,
-                 "City": City,
-                 "Place": Place,
-                 "Review": Review,
-                 "State": State,
-                 "Amenity": Amenity}
+    CLASS_DICT = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "City": City,
+        "Place": Place,
+        "Review": Review,
+        "State": State,
+        "Amenity": Amenity
+    }
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -36,8 +27,7 @@ class FileStorage:
     def save(self):
         """Serializes __objects to the JSON file"""
         with open(FileStorage.__file_path, 'w') as f:
-            dict_obj = {key: value.to_dict()
-                        for key, value in FileStorage.__objects.items()}
+            dict_obj = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
             json.dump(dict_obj, f)
 
     def reload(self):
@@ -45,10 +35,9 @@ class FileStorage:
         if path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as f:
                 objs = json.load(f)
-            for key, value in objs.items():
-                cls_name = value["__class__"]
-                cls = FileStorage.CLASS_DICT.get(cls_name)
-                if cls:
-                    instance = cls(**value)
-                    FileStorage.__objects[key] = instance
-                    
+                for key, value in objs.items():
+                    cls_name = value["__class__"]
+                    cls = FileStorage.CLASS_DICT.get(cls_name)
+                    if cls:
+                        instance = cls(**value)
+                        FileStorage.__objects[key] = instance
